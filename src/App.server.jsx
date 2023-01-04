d5bbc0cd043e41f83e3d748c057f1e15593ac7ce
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-01-04 20:44:17 +0700

    remove partytownjs manual2

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -26,7 +26,7 @@ function App({request}) {
       <ShopifyProvider countryCode={countryCode}>
         <CartProvider countryCode={countryCode}>
           <Suspense>
-            <Partytown debug={true} forward={['dataLayer.push']} />
+            <Partytown debug={false} forward={['dataLayer.push']} />
             <DefaultSeo />
           </Suspense>
           <Router>
